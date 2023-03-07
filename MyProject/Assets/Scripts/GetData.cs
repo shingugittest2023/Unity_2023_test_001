b5939aa257fc85b09ffd1250d2b2a83892b0fb8c
singugittest2023 <singugittest2023@gmail.com>
2023-03-07 14:47:20 +0900

    Excel Data 읽어서 Debug Log 에 표시 했다.

---
+++ MyProject/Assets/Scripts/GetData.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GetData : MonoBehaviour
+{
+    public Entity_GameDB entity_GameDB;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        foreach(Entity_GameDB.Param param in entity_GameDB.sheets[0].list)
+        {
+
+            Debug.Log(param.index + " - " + param.character + " - " + param.Hp + " - " + param.Mp);
+        }
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
